de7a3ae749be689ce857ced48dcdd26d0bc67f3e
AidanaBissenova <47279191+AidanaBissenova@users.noreply.github.com>
2020-03-19 04:47:18 +0600

    Add files via upload

---
+++ week8/informatics/2/a.py
@@ -0,0 +1,4 @@
+a = int(input())
+b= int(input())
+max1=max(a,b);
+print(max1)
---
+++ week8/informatics/2/c.py
@@ -0,0 +1,8 @@
+y = int(input())
+z = int(input())
+if y==z and y==1:
+    print("YES")
+elif z!=1 and y!=1:
+    print("YES")
+else:
+    print("NO")
